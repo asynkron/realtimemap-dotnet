06cc90377bf9381c37afb1e4e6259236527ca1eb
Jakub Grabny <jakub.grabny@skyrise.tech>
2021-07-27 14:00:30 +0200

    Sending MQTT events directly to actor system instead of through channels

--- Backend/MQTT/Ingress.cs
+++ Backend/MQTT/Ingress.cs
@@ -1,13 +1,15 @@
+using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Security.Authentication;
 using System.Text;
-using System.Threading.Channels;
 using System.Threading.Tasks;
 using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Client.Options;
+using MQTTnet.Diagnostics;
 using Newtonsoft.Json;
+using Proto;
+using Proto.Cluster;
 
 namespace Backend.MQTT
 {
@@ -15,87 +17,131 @@ namespace Backend.MQTT
     {
         public bool DoorsOpen { get; set; }
     }
+
     public static class MqttIngress
     {
-        public static async Task<IAsyncEnumerable<Position>> Start()
+        public static async Task Start(Cluster cluster)
         {
-            var factory = new MqttFactory();
-            var mqttClient = factory.CreateMqttClient();
-            var channel = Channel.CreateUnbounded<Position>();
+            var state = new ConcurrentDictionary<string, VehicleState>();
 
-            var options = new MqttClientOptionsBuilder()
+            var mqttClient = CreateMqttClient();
+
+            var mqttClientOptions = new MqttClientOptionsBuilder()
                 .WithClientId("Client1")
-                .WithTls(new MqttClientOptionsBuilderTlsParameters()
+                .WithTls(new MqttClientOptionsBuilderTlsParameters
                 {
                     UseTls = true,
-                    SslProtocol = SslProtocols.Tls12,
+                    SslProtocol = SslProtocols.Tls12
                 })
                 .WithCleanSession()
                 .WithTcpServer("mqtt.hsl.fi", 8883)
                 .Build();
 
-            await mqttClient.ConnectAsync(options);
-            await mqttClient.SubscribeAsync("/hfp/v2/journey/#");
-            var state = new ConcurrentDictionary<string, VehicleState>();
-            mqttClient.UseApplicationMessageReceivedHandler(async e =>
+            mqttClient.UseApplicationMessageReceivedHandler(ProcessIncomingMessage(state, cluster));
+            mqttClient.UseConnectedHandler(async args =>
             {
-                //0/1       /2        /3             /4              /5           /6               /7            /8               /9         /10            /11        /12          /13         /14             /15       /16
-                // /<prefix>/<version>/<journey_type>/<temporal_type>/<event_type>/<transport_mode>/<operator_id>/<vehicle_number>/<route_id>/<direction_id>/<headsign>/<start_time>/<next_stop>/<geohash_level>/<geohash>/<sid>/#
-                
-                var payload = e.ApplicationMessage.Payload;
-                var ascii = Encoding.ASCII.GetString(payload);
-                var typed = JsonConvert.DeserializeObject<Root>(ascii);
-                var parts = e.ApplicationMessage.Topic.Split("/");
-                var operatorId = parts[7];
-                var vehicleNumber = parts[8];
+                Console.WriteLine("### CONNECTED WITH MQTT SERVER ###");
 
-                var vehicleId = operatorId + "." + vehicleNumber;
+                await mqttClient.SubscribeAsync("/hfp/v2/journey/#");
 
-                
-                //state should not be handled here, it should be in the respective actor
-                var vs = state.GetOrAdd(vehicleId, x => new VehicleState());
+                Console.WriteLine("### SUBSCRIBED ###");
+            });
+            mqttClient.UseDisconnectedHandler(async args =>
+            {
+                Console.WriteLine("### DISCONNECTED FROM MQTT SERVER ###");
+                if (args.Exception is not null) Console.WriteLine(args.Exception);
 
-                Payload pl = null;
-
-                //the payload from MQTT here is super weird.
-                //there is a property on the root object which dictates what type of event this is
-                //e.g DOC = doors closed, DOO = doors opened. VP = vehicle position
-                if (typed.DoorsOpen != null)
+                try
                 {
-                    pl = typed.DoorsOpen;
-                    vs.DoorsOpen = true;
+                    await mqttClient.ConnectAsync(mqttClientOptions);
                 }
-                
-                if (typed.DoorsClosed != null)
+                catch
                 {
-                    pl = typed.DoorsClosed;
-                    vs.DoorsOpen = false;
+                    Console.WriteLine("### RECONNECTING FAILED ###");
                 }
-
-                if (typed.VehiclePosition != null)
-                {
-                    pl = typed.VehiclePosition;
-                }
-
-                if (pl != null)
-                {
-                    var p = new Position()
-                    {
-                        OrgId = operatorId,
-                        Longitude = pl.Long,
-                        Latitude = pl.Lat,
-                        VehicleId = vehicleId,
-                        Heading = (int) pl.Hdg,
-                        DoorsOpen = vs.DoorsOpen,
-                    };
-
-                    //this should instead forward to vehicle actors
-                    await channel.Writer.WriteAsync(p);
-                }
-                //Console.WriteLine($"{vehicleId}: {typed.Vp.Lat}, {typed.Vp.Long}, {typed.Vp.Hdg}");
             });
 
-            return channel.Reader.ReadAllAsync();
+            await mqttClient.ConnectAsync(mqttClientOptions);
+        }
+
+        private static IMqttClient CreateMqttClient()
+        {
+            var logger = new MqttNetLogger();
+            logger.LogMessagePublished += (sender, e) =>
+            {
+                if (e.LogMessage.Level >= MqttNetLogLevel.Warning) Console.WriteLine(e.LogMessage.Message);
+
+                if (e.LogMessage.Level == MqttNetLogLevel.Error) Console.WriteLine(e.LogMessage.Exception);
+            };
+
+            var factory = new MqttFactory(logger);
+            var mqttClient = factory.CreateMqttClient();
+            return mqttClient;
+        }
+
+        private static Func<MqttApplicationMessageReceivedEventArgs, Task> ProcessIncomingMessage(
+            ConcurrentDictionary<string, VehicleState> state, Cluster cluster)
+        {
+            return async e =>
+            {
+                try
+                {
+                    //0/1       /2        /3             /4              /5           /6               /7            /8               /9         /10            /11        /12          /13         /14             /15       /16
+                    // /<prefix>/<version>/<journey_type>/<temporal_type>/<event_type>/<transport_mode>/<operator_id>/<vehicle_number>/<route_id>/<direction_id>/<headsign>/<start_time>/<next_stop>/<geohash_level>/<geohash>/<sid>/#
+
+                    var payload = e.ApplicationMessage.Payload;
+                    var ascii = Encoding.ASCII.GetString(payload);
+                    var typed = JsonConvert.DeserializeObject<Root>(ascii);
+                    var parts = e.ApplicationMessage.Topic.Split("/");
+                    var operatorId = parts[7];
+                    var vehicleNumber = parts[8];
+
+                    var vehicleId = operatorId + "." + vehicleNumber;
+
+                    //state should not be handled here, it should be in the respective actor
+                    var vs = state.GetOrAdd(vehicleId, x => new VehicleState());
+
+                    Payload pl = null;
+
+                    //the payload from MQTT here is super weird.
+                    //there is a property on the root object which dictates what type of event this is
+                    //e.g DOC = doors closed, DOO = doors opened. VP = vehicle position
+                    if (typed.DoorsOpen != null)
+                    {
+                        pl = typed.DoorsOpen;
+                        vs.DoorsOpen = true;
+                    }
+
+                    if (typed.DoorsClosed != null)
+                    {
+                        pl = typed.DoorsClosed;
+                        vs.DoorsOpen = false;
+                    }
+
+                    if (typed.VehiclePosition != null) pl = typed.VehiclePosition;
+
+                    if (pl != null && pl.HasValidPosition)
+                    {
+                        var p = new Position
+                        {
+                            OrgId = operatorId,
+                            Longitude = pl.Long.Value,
+                            Latitude = pl.Lat.Value,
+                            VehicleId = vehicleId,
+                            Heading = (int) pl.Hdg.Value,
+                            DoorsOpen = vs.DoorsOpen
+                        };
+
+                        await cluster
+                            .GetVehicleActor(p.VehicleId).OnPosition(p, CancellationTokens.FromSeconds(1));
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                    throw;
+                }
+            };
         }
     }
 }
--- Backend/Services/MapBackendService.cs
+++ Backend/Services/MapBackendService.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Backend.Actors;
@@ -7,13 +6,14 @@ using Grpc.Core;
 using Microsoft.Extensions.Logging;
 using Proto;
 using Proto.Cluster;
+using Channel = System.Threading.Channels.Channel;
 
 namespace Backend.Services
 {
     public class MapBackendService : MapBackend.MapBackendBase
     {
-        private readonly ILogger<MapBackendService> _logger;
         private readonly Cluster _cluster;
+        private readonly ILogger<MapBackendService> _logger;
         private readonly ActorSystem _system;
 
         public MapBackendService(ILogger<MapBackendService> logger, Cluster cluster)
@@ -21,44 +21,19 @@ namespace Backend.Services
             _logger = logger;
             _cluster = cluster;
             _system = cluster.System;
-            
-            //TODO: this should be moved to elsewhere
-            _ = StartConsumer();
         }
 
-        //TODO: this should be moved to elsewhere
-        private async Task StartConsumer()
+        public override async Task Connect(IAsyncStreamReader<CommandEnvelope> requestStream,
+            IServerStreamWriter<PositionBatch> responseStream, ServerCallContext context)
         {
-            await Task.Yield();
-            var positions = await MqttIngress.Start();
-            var batches = positions.Buffer(10);
-            await foreach (var batch in batches)
-            {
-                try
-                {
-                    var requests = batch
-                        .Select(m => _cluster
-                            .GetVehicleActor(m.VehicleId).OnPosition(m, CancellationTokens.FromSeconds(1)))
-                        .ToList();
+            await MqttIngress.Start(_cluster);
 
-                    await Task.WhenAll(requests);
-                }
-                catch(Exception x)
-                {
-                    //There is a bug in the typed client atm, we are getting not supported exception sometimes
-                    _logger.LogError(x, "Mqtt reader failed");
-                }
-            }
-        }
-
-        public override async Task Connect(IAsyncStreamReader<CommandEnvelope> requestStream, IServerStreamWriter<PositionBatch> responseStream, ServerCallContext context)
-        {
             //this is a channel for all events for this specific request
-            var positionsChannel = System.Threading.Channels.Channel.CreateUnbounded<Position>();
+            var positionsChannel = Channel.CreateUnbounded<Position>();
             var props = Props.FromProducer(() => new ViewportActor(positionsChannel));
             //this is out viewport actor for this request
             var viewportPid = _cluster.System.Root.Spawn(props);
-            
+
             //subscribe to all position events, so that our viewport actor receives all those positions
             var sub = _system.EventStream.Subscribe<Position>(_system.Root, viewportPid);
 
