89624099fb3a29da78ae360c2638dad62d8e8373
Ryszard Tarajkowski <ryszard.tarajkowski@skyrise.tech>
2021-09-07 10:15:08 +0200

    Vehicles saves only limited position history.

--- Backend/Actors/VehicleActor.cs
+++ Backend/Actors/VehicleActor.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Proto;
 
@@ -7,17 +5,15 @@ namespace Backend.Actors
 {
     public class VehicleActor : VehicleActorBase
     {
-        private Position? _currentPosition;
-        private readonly List<Position> _positionsHistory;
+        private readonly VehiclePositionHistory _positionsHistory;
         
         public VehicleActor(IContext context) : base(context)
         {
-            _positionsHistory = new List<Position>();
+            _positionsHistory = new VehiclePositionHistory();
         }
 
         public override Task OnPosition(Position position)
         {
-            _currentPosition = position;
             _positionsHistory.Add(position);
             
             //broadcast event on all cluster members eventstream
@@ -30,7 +26,7 @@ namespace Backend.Actors
         public override Task<PositionBatch> GetPositionsHistory(GetPositionsHistoryRequest request)
         {
             var result = new PositionBatch();
-            result.Positions.AddRange(_positionsHistory);
+            result.Positions.AddRange(_positionsHistory.Positions);
 
             return Task.FromResult(result);
         }
---
+++ Backend/Actors/VehiclePositionHistory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Backend.Actors
+{
+    public class VehiclePositionHistory
+    {
+        private const int Capacity = 100;
+        
+        // linked list is used for fast first item removing
+        private readonly LinkedList<Position> _positions = new();
+        
+        public IEnumerable<Position> Positions => _positions;
+        
+        public void Add(Position position)
+        {
+            _positions.AddLast(position);
+
+            if (_positions.Count > Capacity)
+            {
+                _positions.RemoveFirst();
+            }
+        }
+    }
+}
