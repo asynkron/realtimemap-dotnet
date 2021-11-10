8ce0b671087dd68cc05f5ea1a4e73f75b3f128c3
Marcin Budny <marcin.budny@gmail.com>
2021-11-10 13:58:13 +0100

    small cleanup

--- Backend/Actors/OrganizationActor.cs
+++ Backend/Actors/OrganizationActor.cs
@@ -1,69 +1,68 @@
 using Backend.Models;
 
-namespace Backend.Actors
+namespace Backend.Actors;
+
+public class OrganizationActor : OrganizationActorBase
 {
-    public class OrganizationActor : OrganizationActorBase
+    private string _organizationName;
+
+    public OrganizationActor(IContext context) : base(context)
     {
-        private string _organizationName;
-        
-        public OrganizationActor(IContext context) : base(context)
+    }
+
+    public override Task OnStarted()
+    {
+        var organizationId = Context.Self.Id.Substring("partition-activator/".Length, 4);
+
+        var organization = GetOrganization(organizationId);
+
+        _organizationName = organization.Name;
+
+        Console.WriteLine($"Started actor for organization: {organizationId} -- {_organizationName}");
+
+        foreach (var geofence in organization.Geofences)
         {
+            CreateGeofenceActor(geofence);
         }
 
-        public override Task OnStarted()
-        {
-            var organizationId = Context.Self.Id.Substring("partition-activator/".Length, 4);
+        return Task.CompletedTask;
+    }
 
-            var organization = GetOrganization(organizationId);
+    private static Organization GetOrganization(string organizationId)
+    {
+        return Organizations.ById.TryGetValue(organizationId, out var foundOrganization)
+            ? foundOrganization
+            : new Organization(organizationId, organizationId);
+    }
 
-            _organizationName = organization.Name;
+    private void CreateGeofenceActor(CircularGeofence circularGeofence)
+    {
+        var geofenceProps = Props.FromProducer(() => new GeofenceActor(
+            _organizationName,
+            circularGeofence,
+            Cluster
+        ));
 
-            Console.WriteLine($"Started actor for organization: {organizationId} -- {_organizationName}");
+        Context.Spawn(geofenceProps);
+    }
 
-            foreach (var geofence in organization.Geofences)
-            {
-                CreateGeofenceActor(geofence);
-            }
+    public override Task OnPosition(Position request)
+    {
+        foreach (var child in Context.Children) Context.Send(child, request);
 
-            return Task.CompletedTask;
-        }
+        return Task.CompletedTask;
+    }
 
-        private static Organization GetOrganization(string organizationId)
-        {
-            return Organizations.ById.TryGetValue(organizationId, out var foundOrganization)
-                ? foundOrganization
-                : new Organization(organizationId, organizationId);
-        }
+    public override async Task<GetGeofencesResponse> GetGeofences(GetGeofencesRequest request)
+    {
+        var result = new GetGeofencesResponse();
 
-        private void CreateGeofenceActor(CircularGeofence circularGeofence)
-        {
-            var geofenceProps = Props.FromProducer(() => new GeofenceActor(
-                _organizationName,
-                circularGeofence,
-                Cluster
-            ));
+        var tasks = Context.Children
+            .Select(child => Context.RequestAsync<GeofenceDetails>(child, request))
+            .ToList();
 
-            Context.Spawn(geofenceProps);
-        }
+        foreach (var task in tasks) result.Geofences.Add(await task);
 
-        public override Task OnPosition(Position request)
-        {
-            foreach (var child in Context.Children) Context.Send(child, request);
-
-            return Task.CompletedTask;
-        }
-
-        public override async Task<GetGeofencesResponse> GetGeofences(GetGeofencesRequest request)
-        {
-            var result = new GetGeofencesResponse();
-            
-            var tasks = Context.Children
-                .Select(child => Context.RequestAsync<GeofenceDetails>(child, request))
-                .ToList();
-
-            foreach (var task in tasks) result.Geofences.Add(await task);
-
-            return result;
-        }
+        return result;
     }
 }
